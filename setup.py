43e6976a962a22f2e2528cfdc501479a7d9423da
Thinh Nguyen <thinh@vathes.com>
2021-03-22 16:17:02 -0500

    fix typo

--- setup.py
+++ setup.py
@@ -13,13 +13,13 @@ with open(path.join(here, 'requirements.txt')) as f:
 setup(
     name='element-session',
     version='0.0.1',
-    description="DataJoint Elements for Session Management",
+    description="DataJoint Element for Session Management",
     long_description=long_description,
     author='DataJoint NEURO',
     author_email='info@vathes.com',
     license='MIT',
     url='https://github.com/datajoint/element-session',
-    keywords='neuroscience session manamgement datajoint',
+    keywords='neuroscience session-management datajoint',
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     install_requires=requirements,
 )
