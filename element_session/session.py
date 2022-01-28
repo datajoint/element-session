546bf54e7f68b7d263294662e27f8e3b34b896ef
Chris Broz <CBroz@datajoint.com>
2022-01-28 12:47:22 -0600

    Dockerfile: dja->anaconda. Replace missing create_schema docstring

--- element_session/session.py
+++ element_session/session.py
@@ -12,6 +12,8 @@ def activate(schema_name, create_schema=True, create_tables=True,
     activate(schema_name, create_schema=True, create_tables=True, linking_module=None)
         :param schema_name: schema name on the database server to activate
                             the `session` element
+        :param create_schema: when True (default), create schema in the
+                              database if it does not yet exist.
         :param create_tables: when True (default), create tables in the
                               database if they do not yet exist.
         :param linking_module: a module name or a module containing the
