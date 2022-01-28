08d7ef71327e4f2c978f555bf8177ece787187fb
Chris Broz <CBroz@datajoint.com>
2022-01-28 11:26:05 -0600

    PEP8. Add diagram + Experimenter, Note tables

--- element_session/session.py
+++ element_session/session.py
@@ -3,24 +3,31 @@ import importlib
 import inspect
 
 schema = dj.schema()
+_linking_module = None
 
 
-def activate(schema_name, create_schema=True, create_tables=True, linking_module=None):
+def activate(schema_name, create_schema=True, create_tables=True,
+             linking_module=None):
     """
     activate(schema_name, create_schema=True, create_tables=True, linking_module=None)
-        :param schema_name: schema name on the database server to activate the `session` element
-        :param create_schema: when True (default), create schema in the database if it does not yet exist.
-        :param create_tables: when True (default), create tables in the database if they do not yet exist.
+        :param schema_name: schema name on the database server to activate
+                            the `session` element
+        :param create_tables: when True (default), create tables in the
+                              database if they do not yet exist.
         :param linking_module: a module name or a module containing the
          required dependencies to activate the `session` element:
              Upstream tables:
-                + Subject: the subject for which a particular experimental session is associated with
-                + Project: the project for which experimental sessions are associated with
+                + Subject: the subject with which an experimental session is associated
+                + Project: the project with which experimental sessions are associated
+                + Experimenter: the experimenter(s) participating in a given session
     """
     if isinstance(linking_module, str):
         linking_module = importlib.import_module(linking_module)
     assert inspect.ismodule(linking_module),\
         "The argument 'dependency' must be a module's name or a module"
+
+    global _linking_module
+    _linking_module = linking_module
 
     schema.activate(schema_name, create_schema=create_schema,
                     create_tables=create_tables, add_objects=linking_module.__dict__)
@@ -30,7 +37,7 @@ def activate(schema_name, create_schema=True, create_tables=True, linking_module
 class Session(dj.Manual):
     definition = """
     -> Subject
-    session_datetime: datetime(3)
+    session_datetime: datetime
     """
 
 
@@ -39,7 +46,24 @@ class SessionDirectory(dj.Manual):
     definition = """
     -> Session
     ---
-    session_dir: varchar(256)       # Path to the data directory for a particular session
+    session_dir: varchar(256) # Path to the data directory for a session
+    """
+
+
+@schema
+class SessionExperimenter(dj.Manual):
+    definition = """
+    -> Session
+    -> Experimenter
+    """
+
+
+@schema
+class SessionNote(dj.Manual):
+    definition = """
+    -> Session
+    ---
+    session_note: varchar(1024)
     """
 
 
