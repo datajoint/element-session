17c3d52cf8122c34b2570d90f899f846c466bb93
bendichter <ben.dichter@gmail.com>
2022-02-21 20:23:58 -0600

    fix test indenting

--- element_session/export/test_nwb.py
+++ element_session/export/test_nwb.py
@@ -5,18 +5,21 @@ from element_session.export.nwb import session_to_nwb
 
 def test_session_to_nwb():
 
-    session_key = {
-        'subject': 'subject5',
-        'session_datetime': datetime.datetime(2020, 5, 12, 4, 13, 7)
-    }
-lab_key = {'lab': 'LabA'}
-protocol_key = {'protocol': 'ProtA'}
-project_key = {'project': 'ProjA'}
-    nwbfile = session_to_nwb(session_key, lab_key=lab_key, protocol_key=protocol_key, project_key=project_key)
+    nwbfile = session_to_nwb(
+        {
+            "subject": "subject5",
+            "session_datetime": datetime.datetime(2020, 5, 12, 4, 13, 7),
+        },
+        lab_key={"lab": "LabA"},
+        protocol_key={"protocol": "ProtA"},
+        project_key={"project": "ProjA"},
+    )
 
-    assert nwbfile.session_id == 'subject5_2020-05-12T04:13:07'
+    assert nwbfile.session_id == "subject5_2020-05-12T04:13:07"
     assert nwbfile.session_description == "Test"
-    assert nwbfile.session_start_time == datetime.datetime(2020, 5, 12, 4, 13, 7, tzinfo=datetime.timezone.utc)
+    assert nwbfile.session_start_time == datetime.datetime(
+        2020, 5, 12, 4, 13, 7, tzinfo=datetime.timezone.utc
+    )
     assert nwbfile.experimenter == ["User1"]
 
     assert nwbfile.subject.subject_id == "subject5"
